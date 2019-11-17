7a99464a13d486e3f5f4c9a7a25dd013d8c5e5bd
ayil <1239423535@qq.com>
2019-11-17 23:05:20 +0800

    首页处理

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import "@/assets/css/reset.css";
+import "@/assets/js/rem.js";
+import "vant/lib/index.css";
+
+import {
+  Button,
+  Icon,
+  NavBar,
+  Tabbar,
+  TabbarItem,
+  Swipe,
+  SwipeItem,
+  Lazyload
+} from "vant";
+Vue.use(NavBar)
+  .use(Icon)
+  .use(Tabbar)
+  .use(Button)
+  .use(Swipe)
+  .use(TabbarItem)
+  .use(SwipeItem)
+  .use(Lazyload);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
